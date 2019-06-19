2141e09859b770aa48ad02dbdee1ef6177e6c3a2
adudley78 <dudleyadam@gmail.com>
2019-06-19 13:39:46 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
 // Code SimpleComponent Here
+
+import React, { Component } from 'react'
+
+const defaultMood = "happy"
+
+class SimpleComponent extends Component {
+
+    state = {
+        mood: "happy"
+    }
+
+    render() {
+        <div>
+            onClick={this.props.onChange}
+            {this.props.mood || defaultMood}
+        </div>
+    }
+}
+
+handleClick () => {
+    this.event.target.mood = "sad"
+}
+
+export default SimpleComponent
